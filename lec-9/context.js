900510574c372445317a58e7b70722e20e85d487
wali55 <walisantunu@gmail.com>
2023-10-13 12:24:26 +0600

    execution context

--- lec-9/context.js
+++ lec-9/context.js
@@ -1,4 +1,17 @@
-function A() {}
-function B() {
-    
+// execution context
+function A() {
+    console.log('I am A');
 }
+function B() {
+    A();
+}
+function C() {
+    B();
+    B();
+}
+function D() {
+    C();
+    A();
+}
+
+D();
